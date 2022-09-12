08fb8cfd80b7b25c523b971b9fc1711fdac6d7e3
Rod Conan <rconan@gmto.org>
2022-09-11 20:39:50 -0700

    NODAL ABERRATION THEORY CRATE

---
+++ nat/src/mesh.rs
@@ -0,0 +1,28 @@
+//! Generate circular mesh using Delaunay triangulation
+
+use triangle_rs::{Builder, Delaunay};
+
+/// Circular mesh
+///
+/// The mesh is builder based on the diameter odf the enclosing circles
+///  and a maximum triangle area constraint
+pub struct Mesh {
+    diameter: f64,
+    max_area_triangle: f64,
+}
+impl Mesh {
+    pub fn new(diameter: f64, max_area_triangle: f64) -> Builder {
+        let perimeter = std::f64::consts::PI * diameter;
+        let side = (4f64 * max_area_triangle / 3f64.sqrt()).sqrt();
+        let n = (perimeter / side).round() as usize;
+        let r = diameter * 0.5;
+        let vertices: Vec<f64> = (0..n)
+            .map(|i| 2. * i as f64 * std::f64::consts::PI / n as f64)
+            .map(|o| o.sin_cos())
+            .flat_map(|(y, x)| vec![r * x, r * y])
+            .collect();
+        Builder::new()
+            .add_nodes(vertices.as_slice())
+            .set_switches(&format!("qa{:}", max_area_triangle))
+    }
+}
