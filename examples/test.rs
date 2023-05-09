8d9e85e67e335c8170a430228a84d6ff51e8082d
Rod Conan <rconan@gmto.org>
2023-05-09 15:42:07 +0000

    BUMP VERSIONS

--- examples/test.rs
+++ examples/test.rs
@@ -1,8 +1,12 @@
-use crseo::{SourceBuilder,GmtBuilder,Builder};
+use std::time::Instant;
+
+use crseo::{Builder, GmtBuilder, SourceBuilder};
 
 fn main() {
     let mut src = SourceBuilder::default().build().unwrap();
     let mut gmt = GmtBuilder::default().build().unwrap();
+    let now = Instant::now();
     src.through(&mut gmt).xpupil();
+    println!("Ray tracing in {}ms", now.elapsed().as_millis());
     println!("WFE RMS: {}", src.wfe_rms_10e(-9)[0]);
 }
